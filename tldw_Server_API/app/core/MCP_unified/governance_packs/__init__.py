4ca3066fa1bf330880dfff4940fe95692406c3b3
Robert <contact@rmusser.net>
2026-03-13 23:28:45 -0700

    feat: generate governance pack OPA artifacts

--- tldw_Server_API/app/core/MCP_unified/governance_packs/__init__.py
+++ tldw_Server_API/app/core/MCP_unified/governance_packs/__init__.py
@@ -8,16 +8,22 @@ from .models import (
     GovernancePackValidationResult,
     PersonaTemplate,
 )
+from .normalize import NormalizedGovernancePackIR, normalize_governance_pack
+from .opa_bundle import GeneratedGovernancePackBundle, build_opa_bundle
 from .validation import validate_governance_pack
 
 __all__ = [
     "ApprovalTemplate",
     "AssignmentTemplate",
     "CapabilityProfile",
+    "GeneratedGovernancePackBundle",
     "GovernancePack",
     "GovernancePackManifest",
     "GovernancePackValidationResult",
+    "NormalizedGovernancePackIR",
     "PersonaTemplate",
+    "build_opa_bundle",
     "load_governance_pack_fixture",
+    "normalize_governance_pack",
     "validate_governance_pack",
 ]
